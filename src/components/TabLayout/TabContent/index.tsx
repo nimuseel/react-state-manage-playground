aa1fff9305b8439184ce26256f62f3ab93ec176d
nimuseel <l08c0739@gmail.com>
2022-09-22 21:21:46 +0900

    상영 중인 영화 탭에 상영 중인 영화 title 렌더링

--- src/components/TabLayout/TabContent/index.tsx
+++ src/components/TabLayout/TabContent/index.tsx
@@ -1,4 +1,4 @@
-import { TabPanel, TabPanels } from '@chakra-ui/react';
+import { TabPanel, TabPanels, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { Movies } from '../../../types/movies';
 
@@ -13,11 +13,13 @@ export const TabContent = ({
 }: TabContentProps) => {
   return (
     <TabPanels>
-      {nowPlayingMovies.results.map((movie) => (
-        <TabPanel key={movie.id}>
-          <span>{movie.title}</span>
-        </TabPanel>
-      ))}
+      <TabPanel>
+        <VStack>
+          {nowPlayingMovies.results.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </VStack>
+      </TabPanel>
     </TabPanels>
   );
 };
